83037a27804925fa2607bb450dcb5c75f4f06e28
Elmer C <elmer.galo@outlook.com>
2019-10-22 15:39:59 -0400

    Add user input handler events and request API js code

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+$(function() {
+
+    var ingredients = "";
+
+    /* Function makes API request for three recipes. 
+       It uses the ingredients argument as search term. */
+
+    function getRecipe(ingredients) {
+
+        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredients + "&number=3&apiKey=0286efeb2caf42acb2448ecf6ec3249b";
+
+        $.ajax({
+
+            url: queryURL,
+            method: "GET"
+
+        }).then(function (response) {
+
+            console.log(response);
+
+        });
+
+    }
+
+    /* Add click event to input-ingredients__button */
+
+    $("#input-ingredients__button").click(function () {
+
+        /* Store user input ingredients in global variable */
+
+        ingredients = $("#user-input-ingredients").val();
+
+        /* Clear user ingredients input */
+
+        $("#user-input-ingredients").val("");
+
+        /* Call getRecipe function to make API request with ingredients argument */
+
+        //getRecipe(ingredients); /* Uncomment for testing API only  */
+
+        /* Store list array of ingredients */
+
+        var ingredientsList = ingredients.split(",");
+
+        /* Loop through ingredients list and creates a list element with the ingredient text and
+           then append it to ingredients list HTML */
+
+        ingredientsList.forEach( function( ingredient ) {
+
+            var li = $("<li class='collection-item'>").text(ingredient);
+
+            $(".ingredients-list ul").append(li);
+
+        });
+
+    });
+
+});
