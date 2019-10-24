b50a679d2b4a1f1d4c919861ae9ab7df9e663b25
Elmer C <elmer.galo@outlook.com>
2019-10-24 19:29:44 -0400

    Add Styling css to cards and add recipe description

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     var ingredients = "";
 
@@ -18,10 +18,24 @@ $(function() {
 
             console.log(response);
 
+            $("#recipe-div").empty();
+
+            response.forEach(function (recipe) {
+
+
+                $.ajax({
+
+                    url: "https://api.spoonacular.com/recipes/" + recipe.id + "/summary?apiKey=0286efeb2caf42acb2448ecf6ec3249b",
+                    method: "GET"
+                }).then(function (summary) {
+
+                    $("#recipe-div").append(" <div class='col s12 m7 l4'> <div class='card'> <div class='card-image'> <img src='" + recipe.image + "'><span class='card-title'>" + recipe.title + "</span> </div> <div class='card-content text-size'> <p>" + summary.summary + "</p> </div> <div class='card-action text-size'> <a href='#'>Make This!</a> </div> </div> </div>");
+                });
+
+            });
+
         });
-
     }
-
     /* Add click event to input-ingredients__button */
 
     $("#input-ingredients__button").click(function () {
@@ -41,7 +55,7 @@ $(function() {
         /* Loop through ingredients list and creates a list element with the ingredient text and
            then append it to ingredients list HTML */
 
-        ingredientsList.forEach( function( ingredient ) {
+        ingredientsList.forEach(function (ingredient) {
 
             var li = $("<li class='collection-item'>").text(ingredient);
 
@@ -51,17 +65,15 @@ $(function() {
 
     });
 
-    $("#get-recipe-button").on("click", function() {
+    $("#get-recipe-button").on("click", function () {
 
         /* Call getRecipe function to make API request with ingredients argument */
 
         getRecipe(ingredients); /* Uncomment for testing API only  */
 
         // new
-        $("#recipe-div").empty();
-        $("#recipe-div").append("<div class='row'> <div class='col s12 m7 l4'> <div class='card'> <div class='card-image'> <img src='./assets/burger.jpg'> <span class='card-title'>New Recipe</span> </div> <div class='card-content text-size'> <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p> </div> <div class='card-action text-size'> <a href='#'>Make This!</a> </div> </div> </div> </div>")
+
         // new
-        
     })
 
 });
--- assets/javascript/joke.js
+++ assets/javascript/joke.js
@@ -1,20 +1,20 @@
 $(function () {
 
-    var queryURL = "https://api.spoonacular.com/food/jokes/random?apiKey=0286efeb2caf42acb2448ecf6ec3249b"
+    // var queryURL = "https://api.spoonacular.com/food/jokes/random?apiKey=0286efeb2caf42acb2448ecf6ec3249b"
 
-        $.ajax({
+    //     $.ajax({
 
-            url: queryURL,
-            method: "GET"
+    //         url: queryURL,
+    //         method: "GET"
 
-        }).then(function (response) {
+    //     }).then(function (response) {
 
-            console.log(response);
-            console.log(response.text);
+    //         console.log(response);
+    //         console.log(response.text);
 
-            $("#recipe-div").html(response.text);
+    //         $("#recipe-div").html(response.text);
 
-        });
+    //     });
 
    
 
