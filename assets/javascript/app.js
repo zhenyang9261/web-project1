68dcda910e9476cc2e78621c52db69a1d6687220
ctslone <ctslone27@gmail.com>
2019-10-28 16:04:50 -0400

    adding for loops to use with the modals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,7 +21,10 @@ $(function() {
             url: queryURL,
             method: "GET"
 
-        }).then(function (response) {
+        }).then(function (recipe) {
+            // var recipe = recipe.data;
+            console.log(recipe);
+            // console.log(recipe[i].id)
             // response.forEach(function(response) {
             // var objList = {};
             // objList.id = response.id;
@@ -32,15 +35,23 @@ $(function() {
 
             $("#recipe-div").empty();
 
-            response.forEach(function (recipe) {
-
-
+            for (var i=0; i<recipe.length; i++) {
+                console.log(recipe[i].id)
+            // response.forEach(function (recipe) {
                 $.ajax({
 
-                    url: "https://api.spoonacular.com/recipes/" + recipe.id + "/summary?apiKey=88df5afa7d444635ad16e4505c402a69",
+                    url: "https://api.spoonacular.com/recipes/" + recipe[i].id + "/summary?apiKey=88df5afa7d444635ad16e4505c402a69",
                     method: "GET"
-                }).then(function (summary) {
 
+                }).then(function (details) {
+                    console.log(details);
+                    
+                    // for (s=0; s<details.length; s++) {
+                        console.log(details.length);
+                        
+                        console.log(details[i].summary);
+                        // console.log(summary[i].summary);
+                    
                     // $("#recipe-div").append(
                     // `<div class="col s12 m7 l4">\
                     //     <div class="card">\
@@ -59,14 +70,14 @@ $(function() {
                     var card = $('<div class="card">');
                     var cardImage = $('<div class="card-image">')
                     var image = $('<img>')
-                    image.attr('src', recipe.image);
+                    image.attr('src', recipe[i].image);
                     var span = $('<span class="shadow-text card-title">');
-                    span.text(recipe.title);
+                    span.text(recipe[i].title);
                     var fab = $('<button data-target="modal1" class="btn-floating halfway-fab waves-effect waves-light teal modal-trigger"><i class="material-icons">unfold_more</i></button>')
                     var actionDiv = $('<div class="card-action text-size">');
                     var makeThis = $('<a href="#">Make This!</a>');
                     // var modalButton = $("<button data-target='modal1' class='btn modal-trigger'>Summary</button>")
-                    fab.attr('data-summary', summary.summary);
+                    fab.attr('data-summary', summary[i].summary);
                     actionDiv.append([makeThis]);
                     cardImage.append([image, span, fab]);
                     card.append([cardImage, actionDiv]);
@@ -81,15 +92,21 @@ $(function() {
                         var summary = $(this).attr("data-summary");
                         $(".modal-content").append(summary);
                     })
+                    // length loop
+                    // }
 
                     // $("#recipe-div").append("<div class='card col s12 m7 l4'> <div class='card-image waves-effect waves-block waves-light'><img class='activator' src='" + recipe.image + "'><span class='card-title activator text-size shadow-text'>" + recipe.title + "</div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>Summary<i class='material-icons right'>more_vert</i></span> <p><a class='waves-effect waves-light btn'>Make This!</a></p></div><div class='card-reveal'><span class='card-title'>Summary<i class='material-icons right'>close</i></span><p class='text-size'>" + summary.summary + "</p></div></div>")
                     // $("#recipe-div").append("<div class='card'> <div class='card-image waves-effect waves-block waves-light'><img class='activator' src='images/office.jpg'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>Card Title<i class='material-icons right'>more_vert</i></span><p><a href='#'>This is a link</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>Card Title<i class='material-icons right'>close</i></span> <p>Here is some more information about this product that is only revealed once clicked on.</p> </div></div>")
                 });
-
-            });
+            // for each
+            // });
+            // reg loop
+            };
+            
 
         });
     }
+
     /* Add click event to input-ingredients__button */
 
     $("#input-ingredients__button").click(function () {
