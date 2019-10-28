4919a61c884eb8b4616dd0b7342c038f48076c99
ctslone <ctslone27@gmail.com>
2019-10-28 17:25:04 -0400

    added functionality of working modals for each recipe summary. had to also change .append to .html. also cleaned up extra code and comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,38 +22,16 @@ $(function() {
             method: "GET"
 
         }).then(function (response) {
-            // response.forEach(function(response) {
-            // var objList = {};
-            // objList.id = response.id;
-            // objList.img = response.image;
-            // objList.title = response.title;
-            // recipeList.push(objList);
-            // })
 
             $("#recipe-div").empty();
 
             response.forEach(function (recipe) {
-
 
                 $.ajax({
 
                     url: "https://api.spoonacular.com/recipes/" + recipe.id + "/summary?apiKey=88df5afa7d444635ad16e4505c402a69",
                     method: "GET"
                 }).then(function (summary) {
-
-                    // $("#recipe-div").append(
-                    // `<div class="col s12 m7 l4">\
-                    //     <div class="card">\
-                    //         <div class="card-image"> \
-                    //             <img src="` + recipe.image + `">\
-                    //             <span class="shadow-text card-title">` + recipe.title + `</span>\
-                    //         </div> \
-                    //         <div class="card-action text-size">\
-                    //             <a href="#">Make This!</a> \
-                    //             <button data-target="modal1" class="btn modal-trigger" data-summary="`+summary.summary+`">Modal</button>\
-                    //         </div>\
-                    //   </div>\
-                    // </div>`);
 
                     var column = $('<div class="col s12 m7 l4">')
                     var card = $('<div class="card">');
@@ -65,25 +43,20 @@ $(function() {
                     var fab = $('<button data-target="modal1" class="btn-floating halfway-fab waves-effect waves-light teal modal-trigger"><i class="material-icons">unfold_more</i></button>')
                     var actionDiv = $('<div class="card-action text-size">');
                     var makeThis = $('<a href="#">Make This!</a>');
-                    // var modalButton = $("<button data-target='modal1' class='btn modal-trigger'>Summary</button>")
                     fab.attr('data-summary', summary.summary);
                     actionDiv.append([makeThis]);
                     cardImage.append([image, span, fab]);
                     card.append([cardImage, actionDiv]);
                     column.append(card);
                     
-
                     $("#recipe-div").append(column);
 
                     $(".modal-trigger").on("click", function() {
                         console.log(this);
-                        // console.log($(this).data("summary"))
                         var summary = $(this).attr("data-summary");
-                        $(".modal-content").append(summary);
+                        $(".modal-content").html("<h4> Recipe Summary </h4>" + summary);
                     })
 
-                    // $("#recipe-div").append("<div class='card col s12 m7 l4'> <div class='card-image waves-effect waves-block waves-light'><img class='activator' src='" + recipe.image + "'><span class='card-title activator text-size shadow-text'>" + recipe.title + "</div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>Summary<i class='material-icons right'>more_vert</i></span> <p><a class='waves-effect waves-light btn'>Make This!</a></p></div><div class='card-reveal'><span class='card-title'>Summary<i class='material-icons right'>close</i></span><p class='text-size'>" + summary.summary + "</p></div></div>")
-                    // $("#recipe-div").append("<div class='card'> <div class='card-image waves-effect waves-block waves-light'><img class='activator' src='images/office.jpg'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>Card Title<i class='material-icons right'>more_vert</i></span><p><a href='#'>This is a link</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>Card Title<i class='material-icons right'>close</i></span> <p>Here is some more information about this product that is only revealed once clicked on.</p> </div></div>")
                 });
 
             });
@@ -130,12 +103,6 @@ $(function() {
 
         // new
     })
-
-    
-
-    // <div id='modal1' class='modal'> <div class='modal-content'> <h4>Modal Header</h4> <p>A bunch of text</p> </div> <div class='modal-footer'><a href='#!' class='modal-close waves-effect waves-green btn-flat'>Agree</a> </div>
-
-   
 
 });
 
