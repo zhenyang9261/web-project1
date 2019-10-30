d5acbf3699cedac411e833f90755d4519d912691
Elmer C. Galo <elmer.galo@outlook.com>
2019-10-30 17:33:07 -0400

    Add comment to joke.js file

--- assets/javascript/joke.js
+++ assets/javascript/joke.js
@@ -1,3 +1,4 @@
+/* Makes API call to get random joke and append it to HTML initial page load */
 $(function () {
 
     var queryURL = "https://api.spoonacular.com/food/jokes/random?apiKey=b52ff59b76aa49f2873c01370b4d2a33"
@@ -9,14 +10,7 @@ $(function () {
 
         }).then(function (response) {
 
-            console.log(response);
-            console.log(response.text);
-
-            $("#recipe-div").html("<h5>Please enter your ingredinets to begin. For now, here is a joke!</h5>" + "<hr>" + "<p id='joke-text'>" + response.text + "</p>");
+            $("#recipe-div").html("<h5>Please enter your ingredients to begin. For now, here is a joke!</h5>" + "<hr>" + "<p id='joke-text'>" + response.text + "</p>");
 
         });
-
-   
-
-
-})
+});
