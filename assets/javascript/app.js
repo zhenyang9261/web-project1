459922847cc1bec0803641ecdc6bb61379db157f
Elmer C. Galo <elmer.galo@outlook.com>
2019-10-30 17:28:27 -0400

    Add comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,11 @@
+/* Store recipe's Ids */
 var idList = [];
 
 $(document).ready(function(){
-    // open the modal html on click
+    /* Open the modal html on click to show recipe description*/
     $('.modal').modal();
 
-// global varible for storing the recipe IDs retunred
-var recipeList = []
-
-
-
+    /* Store input ingredient's name */
     var ingredients = [];
 
     /* Function makes API request for three recipes. 
@@ -56,82 +53,65 @@ var recipeList = []
                     $("#recipe-div").append(column);
 
                     $(".modal-trigger").on("click", function() {
-                        console.log(this);
                         var summary = $(this).attr("data-summary");
                         $(".modal-content").html("<h4> Recipe Summary </h4>" + summary);
                     })
-
                 });
-
             });
-
-            // Call functions to create object with instructions list
+            /* Call functions to create object containing all the recipe's instructions */
             getInstructions();
-
         });
     }
 
     /* Add click event to input-ingredients__button */
-
     $("#input-ingredients__button").click(function () {
-
-        // emptying the ingredients list to allow for extra enttries if user adds anothe item after clicking the get recipe buttons
+        /* emptying the ingredients list to allow for extra entries if user adds another item after clicking the get recipe buttons */
         $(".ingredients-list ul").empty();
 
-        // Store user input ingredients in local variable and push to global variable 
+        /* Store user input ingredients in local variable and push to global variable  */
         var input = $("#user-input-ingredients").val();
         ingredients.push(input);
 
         /* Clear user ingredients input */
-
         $("#user-input-ingredients").val("");
 
         /* Store list array of ingredients */
-
         var ingredientsList = ingredients.toString().split(",");
 
         /* Loop through ingredients list and creates a list element with the ingredient text and
            then append it to ingredients list HTML */
-
         ingredientsList.forEach(function (ingredient) {
-
             var li = $("<li class='collection-item'>").html(ingredient);
-
-
             $(".ingredients-list ul").append(li);
         });
-        
     });
 
     $("#get-recipe-button").on("click", function () {
-
         /* Call getRecipe function to make API request with ingredients argument */
-
-        getRecipe(ingredients); /* Uncomment for testing API only  */
-
+        getRecipe(ingredients);
     });
 
-    // Add click event to back-button
+    /* Add click event to back-button */
     $("#back-button").on("click", function (e) {
         e.preventDefault();
-        // Toggle animation effect on recipe-instructions and recipe-div
+
+        /* Toggle animation effect on recipe-instructions and recipe-div */
         $("#recipe-instructions").toggle("fold");
         $("#recipe-div").toggle("fold");
-
     });
 
     $("#recipe-instructions").hide();
 
-    // Variable store instructions list
+    /* Variable store instructions list */
     var instructionsObj = {};
 
-    // Function creates a list of recipe instructions
+    /* Function creates a list of recipe instructions */
     function getInstructions() {
 
-        // Loop through id list
+        /* Loop through id list */
         idList.forEach(function (id) {
 
-            // Create instructions list data structure
+            /* Create instructions list data structure */
             instructionsObj[id] = { steps: [] };
 
             $.ajax({
@@ -140,28 +120,28 @@ var recipeList = []
 
             }).then(function (instructions) {
 
-                // Get instructions from API response and store it in global variable
+                /* Get instructions from API response and store it in global variable */
                 instructions[0].steps.forEach(function (step) {
-
                     instructionsObj[id].steps.push(step);
                 });
             });
         });
     }
 
+    /* Add click event to instructionsButton */
     $(document).on("click", "#instructionsButton", function (e) {
         e.preventDefault();
 
-        // Clear instructions list cards
+        /* Clear instructions list cards */
         $("#instructions-list").empty();
 
-        // Create instructions list based on recipe Id
+        /* Create instructions list based on recipe Id */
         instructionsObj[$(this).attr("data-id")].steps.forEach(function (step) {
             $("#instructions-list").append("<li>" + step.step + "</li>");
         });
 
-        // Toggle animation effect on recipe-instructions and recipe-div
-        $("#recipe-instructions").toggle("fold");
-        $("#recipe-div").toggle("fold");
+        /* Toggle animation effect on recipe-instructions and recipe-div */
+        $("#recipe-instructions").toggle("fold"); // Display recipe instructions
+        $("#recipe-div").toggle("fold"); // Hide recipe cards
     });
 });
